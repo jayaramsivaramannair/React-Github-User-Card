a3ed52790d0e42cf5ef43a149d91f286adf29a88
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-02-12 20:06:26 -0500

    Installed create-react-app and set up the project with tailwind css for styling

---
+++ github-user-card/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-center text-pink-700">Hello from Github User Card!</p>
+    </div>
+  );
+}
+
+export default App;
